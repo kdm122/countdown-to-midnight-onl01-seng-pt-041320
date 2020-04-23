fca8c15e9bfef301f199d4c75961ce3a520c0c73
Kyle McLean <kyledmclean@gmail.com>
2020-04-23 00:40:51 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,28 @@
 #write your code here
+#time = 10
 
-def countdown
+#def countdown(time)
+#  while time > 0
+#    puts "#{time} SECOND(S)!"
+#    time -= 1
+#  end
+#  while time == 0 
+#    puts "HAPPY NEW YEAR!"
+#    time -= 1 
+#  end
+
+#end
+
+#countdown(time)
+
+def countdown(time)
+until time == 0 
+  puts "#{time} SECOND(S)!"
+  time -= 1
+end
+while time == 0
+puts "HAPPY NEW YEAR!"
+time -= 1
+end
+
 end
