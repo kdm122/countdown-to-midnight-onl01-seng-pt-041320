178c27eddc2d22de2e435e1932ff58f8769b9542
Kyle McLean <kyledmclean@gmail.com>
2020-04-23 00:56:40 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,41 @@
 #write your code here
+#time = 10
 
-def countdown
+#def countdown(time)
+#  while time > 0
+#    puts "#{time} SECOND(S)!"
+#    time -= 1
+#  end
+#  while time == 0 
+#    puts "HAPPY NEW YEAR!"
+#    time -= 1 
+#  end
+
+#end
+
+#countdown(time)
+
+
+def countdown(time)
+until time == 0 
+  puts "#{time} SECOND(S)!"
+  time -= 1
+end
+
+return "HAPPY NEW YEAR!"
+#while time == 0
+#puts "HAPPY NEW YEAR!"
+#time -= 1
+#end
+
+end
+
+def countdown_with_sleep(time)
+  until time == 0 
+  puts "#{time} SECOND(S)!"
+  time -= 1
+  sleep(1)
+end
+
+return "HAPPY NEW YEAR!"
 end
---
+++ countdown_test.rb
@@ -0,0 +1,12 @@
+time = 10
+
+while time > 0
+    puts "#{time} SECOND(S)!"
+    time -= 1
+    
+#    if time == 0
+#      puts "HAPPY NEW YEAR!"
+#      break
+#    end
+    
+end
